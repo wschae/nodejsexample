5b2c7bcfe3d6976101563775fcc645d674e517e8
Wonseok Chae <wochae@microsoft.com>
2017-06-11 01:04:32 -0700

    Up to Gulp

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/books', function(req, res) {
+    res.send('Hello Books!');
+});
+
+app.listen(port, function(err) {
+    console.log('running server on port ' + port);
+});
